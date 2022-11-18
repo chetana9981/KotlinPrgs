336143d4e85480a6e7c8defcb11b1987ffe4d595
chetana9981 <101543143+chetana9981@users.noreply.github.com>
2022-11-18 16:48:25 +0530

    Add files via upload

---
+++ src/main/kotlin/variable.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+
+    //var is used to vary/change the value
+
+    var name="chetana"
+    var age=22
+
+    println(name)
+    println(age)
+
+    //reassgining
+
+    age=23
+    println(age)
+
+
+    // using val we cannot modify the value
+    val week=7
+    println(week)
+}
---
+++ src/main/kotlin/variable2.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var name="chetana"
+    println(name)
+
+    var dept:String="hr"
+    println("dept = $dept")
+
+    var age:Int =22
+    println("age = $age")
+
+}
